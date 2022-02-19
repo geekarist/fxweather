622a1c21e844c2a68833525af3bd0ac75c983ee3
geekarist <geekarist@users.noreply.github.com>
2022-02-20 00:56:30 +0100

    Create `catchall`
    fixup! Create `catchall`
    
    fixup! fixup! Create `catchall`

--- src/fxweather/core.clj
+++ src/fxweather/core.clj
@@ -1,18 +1,19 @@
 (ns fxweather.core
-  (:gen-class
-   :extends javafx.application.Application)
-  (:import
-   (javafx.application Application Platform)
-   (javafx.fxml FXMLLoader)
-   (javafx.scene #_Parent Scene)
-   #_(javafx.stage Stage))
+  (:gen-class :extends javafx.application.Application)
+  (:import (javafx.application Application Platform)
+           (javafx.fxml FXMLLoader)
+           (javafx.scene #_Parent Scene)
+           #_(javafx.stage Stage))
   (:require [clojure.java.io :as io]))
 
-(defn -main []
-  (try (Application/launch fxweather.core (into-array String []))
+(defn- catchall [command]
+  (try (command)
        (catch Exception e
          (do (.printStackTrace e)
              (throw e)))))
+
+(defn -main []
+  (catchall #(Application/launch fxweather.core (into-array String []))))
 
 (defn- create-scene! []
   (let [loc (io/resource "ui.fxml")
@@ -23,22 +24,15 @@
 (defn -start [_this primaryStage]
   #_{:clj-kondo/ignore [:inline-def]}
   (def MAIN_STAGE primaryStage)
-  (try
-    (.setScene primaryStage (create-scene!))
-    (.show primaryStage)
-
-    (catch Exception e
-      (do (.printStackTrace e)
-          (throw e)))))
+  (catchall #(do (.setScene primaryStage (create-scene!))
+                 (.show primaryStage))))
 
 (defn reload-ui! []
   (Platform/runLater
-   #(try (.hide MAIN_STAGE)
-         (.setScene MAIN_STAGE (create-scene!))
-         (.show MAIN_STAGE)
-         (catch Exception e
-           (do (.printStackTrace e)
-               (throw e))))))
+   (fn []
+     (catchall #(do (.hide MAIN_STAGE)
+                    (.setScene MAIN_STAGE (create-scene!))
+                    (.show MAIN_STAGE))))))
 
 (comment
   (comment -start)
